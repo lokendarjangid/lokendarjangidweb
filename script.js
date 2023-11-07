9467efd91ca988560cb1cb95a663e399a155b001
lokendar jangid <lokendarjangid1234@gmail.com>
2023-11-07 22:57:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function () {
+    const preloader = document.querySelector(".preloader");
+    const content = document.querySelector(".App");
+    setTimeout(function () {
+        preloader.classList.add("hide-preloader");
+        content.style.display = "block"; // Show the webpage content after the preloader is hidden
+    }, 0); // Replace 3000 with the number of milliseconds you want to delay before hiding the preloader
+});
+
+
+var typed = new Typed(".auto-type", {
+    strings: ["Open Source Contributer", "Python Developer", "Web Developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.querySelector('.navbar');
+  
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 15) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+  });
