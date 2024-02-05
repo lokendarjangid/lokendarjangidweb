ca9c88ec74a4101cf844c66b87d314e742b4e4d8
lokendar jangid <lokendarjangid1234@gmail.com>
2024-02-05 17:16:56 +0530

    flask completed

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 def index():
     
     data = [ # keep it in multiples of 4 for optimal design
-        ('Project 1', 'Illustration', '#','portfolio-1.jpg'),
+        ('AI Virtual Mouse', 'Mouse movements control using hand movements and perform some actions using hand gesturs.', 'https://github.com/lokendarjangid/virtual-mouse-new','https://raw.githubusercontent.com/lokendarjangid/virtual-mouse-new/main/AI-Virtual-Mouse.jpg'),
+        ('Waker Linux', 'Waker is a simple tool to sleep your computer when you are away from it.', 'https://github.com/lokendarjangid/waker_linux','https://raw.githubusercontent.com/lokendarjangid/waker_linux/master/waker-linux.jpg'),
+        ('Discord Bot', 'Discord bot connect with chatgpt3 to give reasponse and chat with you. use dalle-2 to generate image from text.', 'https://github.com/lokendarjangid/dis-bot','https://raw.githubusercontent.com/lokendarjangid/dis-bot/master/discord-bot.jpg'),
         #('Name', 'Description','link','imageName')
     ]
     
-    return flask.render_template('index.html')
+    return flask.render_template('index.html',data=data)
 
 @app.errorhandler(404)
 def page_not_found(e):
